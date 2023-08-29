c21540da6669ae4c73fea360efae5cbdd169756b
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2023-08-28 22:52:04 -0700

    Leaderboard Framework

---
+++ GhostNirvana/Assets/Scripts/GhostNirvana/Netcode/Leaderboard.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System;
+using Dan.Main;
+
+namespace GhostNirvana.Netcode {
+
+public class LeaderBoard {
+    public string publicKey;
+
+    const int Time_SIZE = 4;
+    const int Money_SIZE = 6;
+    const int ApplianceCount_SIZE = 4;
+
+    public struct Record {
+        public string Name;
+        public int Rank;
+        public int Time;
+        public int MoneyAcquired;
+        public int MoneyTakeHome;
+        public int AppliancesRetrieved;
+        public List<int> BuffsTaken;
+    }
+
+    public (bool, Record) Parse(Dan.Models.Entry entry) {
+        string entryData = entry.Extra;
+        Record record = new Record() {
+            Name = entry.Username,
+            Rank = entry.Rank,
+        };
+        int totalLen = Time_SIZE + 2*Money_SIZE + ApplianceCount_SIZE;
+        if (entryData.Length < totalLen)
+            return (false, record); // data is corrupted
+
+        int currentParsingIndex = 0;
+        int.TryParse(s: entryData.Substring(currentParsingIndex, Time_SIZE), 
+            result: out record.Time);
+        currentParsingIndex += Time_SIZE;
+        int.TryParse(s: entryData.Substring(currentParsingIndex, Money_SIZE), 
+            result: out record.MoneyAcquired);
+        currentParsingIndex += Money_SIZE;
+        int.TryParse(s: entryData.Substring(currentParsingIndex, Money_SIZE), 
+            result: out record.MoneyTakeHome);
+        currentParsingIndex += Money_SIZE;
+        int.TryParse(s: entryData.Substring(currentParsingIndex, ApplianceCount_SIZE), 
+            result: out record.AppliancesRetrieved);
+        currentParsingIndex += ApplianceCount_SIZE;
+
+        record.BuffsTaken = new List<int>();
+        string buffsTaken = entryData.Substring(currentParsingIndex);
+        for (int i = 0; i < buffsTaken.Length; i++) {
+            char c = buffsTaken[i];
+            int id = c;
+            record.BuffsTaken.Add(id);
+        }
+
+        return (true, record);
+    }
+
+    public void Save(Record record, Action<bool> regularCallback, Action<string> errorCallback) {
+        string entryData = "";
+
+        entryData += string.Format("{0:0000}", record.Time);
+        entryData += string.Format("{0:0000.00}", record.MoneyAcquired);
+        entryData += string.Format("{0:0000.00}", record.MoneyTakeHome);
+        entryData += string.Format("{0:0000}", record.AppliancesRetrieved);
+        for (int i = 0; i < record.BuffsTaken.Count; i++)
+            entryData += (char) i;
+
+        LeaderboardCreator.UploadNewEntry(
+            publicKey, 
+            username: record.Name,
+            score: record.MoneyTakeHome,
+            extra: entryData,
+            regularCallback,
+            errorCallback
+        );
+    }
+
+    public void Load(int numberOfEntries, Action<List<Record>> callback) {
+        LeaderboardCreator.GetLeaderboard(
+            publicKey,
+            isInAscendingOrder: true,
+            searchQuery: new Dan.Models.LeaderboardSearchQuery() {
+                Skip = 0,
+                Take = numberOfEntries,
+            },
+            callback: (entries) => {
+                List<Record> records = new List<Record>();
+                for (int i = 0; i < entries.Length; i++) {
+                    var (successful, value) = Parse(entries[i]);
+                    records.Add(value);
+                }
+                callback?.Invoke(records);
+            }
+        );
+    }
+}
+
+}
