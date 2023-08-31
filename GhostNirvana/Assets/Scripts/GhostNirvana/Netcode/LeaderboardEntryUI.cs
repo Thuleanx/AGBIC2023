f92024a4e7ed316f52415747b21cef8d3f45e32b
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2023-08-31 00:29:43 -0700

    code and prefab for leaderboard entry

---
+++ GhostNirvana/Assets/Scripts/GhostNirvana/Netcode/LeaderboardEntryUI.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace GhostNirvana.Netcode {
+    public class LeaderboardEntryUI : MonoBehaviour {
+        [SerializeField] TMP_Text Name;
+        [SerializeField] TMP_Text Rank;
+        [SerializeField] TMP_Text MoneyTakeHome;
+        [SerializeField] TMP_Text Time;
+        [SerializeField] Image mostBoughtItem;
+        [SerializeField] List<Buff> buffs;
+
+        public void Set(Leaderboard.Record record) {
+            Name.text = record.Name;
+            Rank.text = record.Rank.ToString();
+            MoneyTakeHome.text = String.Format("{0:C}", record.MoneyAcquired);
+
+            int secondsInMinute = 60;
+            int timeMinutes = record.Time / secondsInMinute;
+            int timeSeconds= record.Time % secondsInMinute;
+
+            Time.text = String.Format("{0:00}:{1:00}", timeMinutes, timeSeconds);
+
+            Dictionary<int, int> acquisitionFrequency = new Dictionary<int, int>();
+            int mostBoughtItemID = -1;
+            foreach (int buffID in record.BuffsTaken) {
+                if (!acquisitionFrequency.ContainsKey(buffID))
+                    acquisitionFrequency[buffID] = 0;
+                acquisitionFrequency[buffID]++;
+
+                if (mostBoughtItemID == -1) {
+                    mostBoughtItemID = buffID;
+                    continue;
+                }
+
+                int fcurrent = acquisitionFrequency[buffID];
+                int fbest = acquisitionFrequency[mostBoughtItemID];
+
+                bool cmp1 = fcurrent >= fbest;
+                bool cmp2 = buffID >= mostBoughtItemID && fcurrent == fbest;
+
+                if (cmp1 || cmp2)
+                    mostBoughtItemID = buffID;
+            }
+
+            mostBoughtItem.enabled = false;
+
+            foreach (Buff buff in buffs) {
+                if (buff.id == mostBoughtItemID) {
+                    mostBoughtItem.enabled = true;
+                    mostBoughtItem.sprite = buff.icon;
+                    break;
+                }
+            }
+        }
+    }
+}
