435e122a5fa3cb32d5833f1ca62b5fa7ef141f7a
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2023-09-07 00:44:46 -0700

    sugar rush vfx implementation

--- GhostNirvana/Assets/Scripts/GhostNirvana/Progression/Buff/TemporaryBuffTracker.cs
+++ GhostNirvana/Assets/Scripts/GhostNirvana/Progression/Buff/TemporaryBuffTracker.cs
@@ -6,21 +6,24 @@ namespace GhostNirvana {
 public class TemporaryBuffTracker : MonoBehaviour {
     [SerializeField] TemporaryBuff buff;
 
+    [SerializeField] bool queryInitialState;
     [SerializeField] UnityEvent OnApply;
     [SerializeField] UnityEvent OnExpire;
 
     void OnEnable() {
-        if (buff) {
-            buff.OnApply.AddListener(_RunOnApplyEvent);
-            buff.OnExpire.AddListener(_RunOnExpireEvent);
+        if (!buff) return;
+        buff.OnApply.AddListener(_RunOnApplyEvent);
+        buff.OnExpire.AddListener(_RunOnExpireEvent);
+        if (queryInitialState) {
+            if (buff.IsActive)  _RunOnApplyEvent();
+            else                _RunOnExpireEvent();
         }
     }
     
     void OnDisable() {
-        if (buff) {
-            buff.OnApply.RemoveListener(_RunOnApplyEvent);
-            buff.OnExpire.RemoveListener(_RunOnExpireEvent);
-        }
+        if (!buff) return;
+        buff.OnApply.RemoveListener(_RunOnApplyEvent);
+        buff.OnExpire.RemoveListener(_RunOnExpireEvent);
     }
 
     void _RunOnApplyEvent() => OnApply?.Invoke();
